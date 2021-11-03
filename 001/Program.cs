54aae7b0adc4557d3b0069e11d626072e02145a5
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-03 13:47:52 +0300

    001 added

---
+++ 001/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] a = {1,2,3,4,5,6,7,8,9};
+int [] b = new int [a.Length];
+
+int c = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] % 2 == 0)
+    {
+        b[c++] = a[i];
+    }
+}
+Console.WriteLine("b ");
+for (int i = 0; i < c; i++)
+{
+    Console.WriteLine($"{b[i]}");
+}
